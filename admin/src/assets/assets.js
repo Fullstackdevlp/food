7c0a4bdaf999b021c39c89446d4ca02181df4fc5
hiba-dev <124171325+Fullstackdevlp@users.noreply.github.com>
2024-08-31 20:50:09 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,9 +1,10 @@
-import logo from './logo.png'
+oimport logo from './logo.png'
 import add_icon from './add_icon_white.png'
 import order_icon from './orders.png'
 import profile_image from './profile.gif'
 import upload_area from './upload.png'
 import parcel_icon from './parcel_icon.png'
+export const url = "https://food-1-pfjq.onrender.com"
 
 export const assets = {
     logo,
